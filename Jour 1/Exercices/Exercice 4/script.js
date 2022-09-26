3e5f3aa54ae69dca1d6ed2aefe82913e4957bc06
Clément <clement.delord@epitech.eu>
2022-09-26 11:46:40 +0200

    ex 5

---
+++ Jour 1/Exercices/Exercice 4/script.js
@@ -0,0 +1,10 @@
+function chaineDeCharactere (x) {
+    let chaine = "";
+    for (let i = 0; i < x; i++) {
+        chaine += '8';
+        // chaine = chaine + '8';
+    }
+    console.log(chaine);
+}
+
+chaineDeCharactere(100);
