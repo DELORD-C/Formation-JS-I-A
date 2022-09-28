84464bdc693f9aaf96d235ed07fcd05764f0fe6a
Clément <clement.delord@epitech.eu>
2022-09-28 11:18:37 +0200

    ex 13 + 14

--- Jour 3/Exercice/Exercice 13/script.js
+++ Jour 3/Exercice/Exercice 13/script.js
@@ -7,19 +7,35 @@
 function generateEM () {
     
     let numbers = [];
+    let p;
     
     //Ici on calcule la grille
+    for (let i = 0; i < 7; i++) {
+        let number;
+        if (i < 2) {
+            number = Math.ceil(Math.random() * 12);
+        }
+        else {
+            number = Math.ceil(Math.random() * 50);
+        }
+        
+        if (numbers.includes(number)) {
+            i--;
+        }
+        else {
+            numbers.push(number);
+        }
+    }
 
 
     if (document.getElementById('content')) {
-        let p = document.getElementById('content')
-        p.innerHTML = numbers[0] + ', ' + numbers[1] + ', ' + numbers[2] + ', ' + numbers[3] + ', ' + numbers[4] + ', ' + numbers[5] + ', ' + numbers[6];
+        p = document.getElementById('content')
     }
     else {
-        let p = document.createElement('p');
+        p = document.createElement('p');
         p.setAttribute('id', 'content');
-        p.innerHTML = numbers[0] + ', ' + numbers[1] + ', ' + numbers[2] + ', ' + numbers[3] + ', ' + numbers[4] + ', ' + numbers[5] + ', ' + numbers[6];
         document.body.append(p);
     }
+    p.innerHTML = numbers[0] + ', ' + numbers[1] + ', ' + numbers[2] + ', ' + numbers[3] + ', ' + numbers[4] + ', ' + numbers[5] + ', ' + numbers[6];
     
 }
