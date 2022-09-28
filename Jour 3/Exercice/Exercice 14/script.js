1e39523175d9490cf1faf21594fa182574d66d17
Clément <clement.delord@epitech.eu>
2022-09-28 15:08:11 +0200

    rs async + ex 15

--- Jour 3/Exercice/Exercice 14/script.js
+++ Jour 3/Exercice/Exercice 14/script.js
@@ -7,3 +7,23 @@ function tourner () {
         img.style.transform = 'rotate(0deg)';
     }, 2000)
 }
+
+let p = document.getElementById("parag");
+
+p.addEventListener('mouseenter', () => {
+    let colors = ['blue', 'red', 'green', 'orange', 'grey'];
+    let rand = Math.floor(Math.random() * colors.length);
+    p.style.color = colors[rand];
+});
+
+p.addEventListener('mouseleave', () => {
+    p.style.color = '';
+});
+
+window.addEventListener('contextmenu', (e) => {
+    e.preventDefault();
+    let context = document.getElementById('context');
+    context.style.display = 'block';
+    context.style.top = e.clientY + 'px';
+    context.style.left = e.clientX + 'px';
+});
