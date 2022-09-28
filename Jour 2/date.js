2b1feef85385371f962a3ad78d0454cc4c8dcf32
Clément <clement.delord@epitech.eu>
2022-09-28 09:30:18 +0200

    ex 11 & 12

--- Jour 2/date.js
+++ Jour 2/date.js
@@ -1,9 +1,19 @@
-let date = new Date();
-let options = {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric'
-}
+// let date = new Date();
+// let options = {
+//     weekday: 'long',
+//     year: 'numeric',
+//     month: 'long',
+//     day: 'numeric'
+// }
 
-console.log(date.toLocaleDateString('fr-FR', options));
+// console.log(date.toLocaleDateString('fr-FR', options));
+
+// console.log(Date.now());
+
+// let price = 14700000.15;
+
+// let dollarUS = Intl.NumberFormat('en-US');
+// let euroFR = Intl.NumberFormat('fr-FR');
+
+// console.log(dollarUS.format(price) + ' $');
+// console.log(euroFR.format(price) + ' €');
---
+++ Jour 3/math.js
@@ -0,0 +1,5 @@
+// console.log(Math.random());
+
+let rand = Math.ceil(Math.random() * 10);
+
+console.log(rand);
